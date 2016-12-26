159ac88464659ac2cf50d6023f2803dd091aca83
Suranart Niamcome <suranartnc@gmail.com>
2016-12-26 18:31:39 +0700

    add simple counter app

--- pages/a.js
+++ pages/a.js
@@ -1,1 +1,25 @@
-export default () => <div>a</div>
+class CounterApp extends React.Component {
+    state = {
+        count: 0
+    }
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            const count = this.state.count + 1
+            this.setState({
+                count,
+            })
+        }, 1000)
+    }
+    componentWillUnmount() {
+        clearInterval(this.intervalId)
+    }
+    render() {
+        return (
+            <div>
+                {this.state.count}
+            </div>
+        ) 
+    }
+}
+
+export default CounterApp
