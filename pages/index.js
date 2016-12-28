896e30a2bc703f4b023c791f74b9389189f519b0
Suranart Niamcome <suranartnc@gmail.com>
2016-12-28 17:31:26 +0700

    try add getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,49 @@
-import React from 'react'
-
 import page from '../hocs/page'
 
-export default page(() => (
-  <div>
-    <div>Homepage</div>
-  </div>
-))
+class Homepage extends React.Component {
+  static async getInitialProps() {
+    const fetching = function() {
+      return new Promise((resolve, reject) => {
+        console.log('fetching...')
+        setTimeout(() => {
+          return resolve([
+            {
+              id: 1,
+              title: 'Title 1'
+            }
+          ])
+        }, 5000)
+      })
+    }
+    const result = await fetching()
+    console.log('result=', result)
+    return {
+      posts: result,
+    }
+  }
+
+  renderPosts() {
+    console.log(this.props)
+    if (this.props.posts !== undefined) {
+      return this.props.posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+          </div>
+        )
+      })
+    }
+    return null
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Homepage</div>
+        {this.renderPosts()}
+      </div>
+    )
+  }
+}
+
+export default page(Homepage)
